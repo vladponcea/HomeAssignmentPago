eb08178772dd5c04894033ead4bf9ac22e9973f2
vladponcea <vladponcea@icloud.com>
2022-08-20 11:45:59 +0300

    Initial Commit

---
+++ Home Assignment Pago/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home Assignment Pago
+//
+//  Created by Vladut Mihai Poncea on 20.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
